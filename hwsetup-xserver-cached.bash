e2447b84cdd40ddfbf5d28766ee9a84f3938952c
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2005-10-22 22:15:33 +0000

    [svn-inject] Installing original source of hwsetup-ng

---
+++ hwsetup-xserver-cached.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# HWSETUP NG - xserver detection (precached mode)
+# (C) 2005 Joerg Schirottke <master@kanotix.com>
+count=0
+unset VIDEO_ID VIDEO_DRIVER
+while read id driver; do
+ VIDEO_ID[$count]="$id"
+ VIDEO_DRIVER[$count]="$driver"
+ ((count++))
+done < <(cut -f2 -d: /usr/share/hwdata/videoaliases)
+((count--))
+SYS=$(cut -f2 -d: /sys/devices/pci*/*/*/modalias)
+unset found 
+for id in $(seq 0 $count); do
+ for sysid in $SYS; do
+  case $sysid in ${VIDEO_ID[$id]}) found=$id; break; esac
+  #if [[ $sysid == ${VIDEO_ID[$id]} ]]; then found=$id; break; fi
+ done
+ [ "$found" ] && break
+done
+echo XSERVER=\"$([ -x /usr/X11R6/bin/Xorg ] && echo Xorg || echo XFree86)\"
+if [ "$found" ]; then
+ echo XMODULE=\"${VIDEO_DRIVER[$found]}\"
+ echo XDESC=\"$(echo $(lspci -d $(echo $sysid|sed 's/sv.*//;s/v0000//;s/d0000/:/')|cut -f4- -d:))\"
+else
+ echo XMODULE=\"vesa\"
+ VGA=$(lspci|grep VGA|cut -f4- -d:)
+ if [ "$VGA" ]; then
+  echo XDESC=\"$VGA\"
+ else 
+  echo XDESC=\"Generic VGA card\"
+ fi
+fi
