5a7c0dccd4b93901cbccdf6458b71ba56df825c0
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2006-11-09 19:15:59 +0000

    hwsetup-ng (0.1.8) unstable; urgency=low
      * NOT RELEASED YET
      * fix lintian errors.
      * fix bashisms.
      * re- indent.
    
     -- Stefan Lippers-Hollmann <s.l-h@gmx.de>  Thu,  9 Nov 2006 20:15:01 +0100

--- hwsetup-xserver-discover.bash
+++ hwsetup-xserver-discover.bash
@@ -1,25 +1,35 @@
 #!/bin/bash
 # HWSETUP NG - xserver detection - discover database variant
 # (C) 2005-2006 Joerg Schirottke <master@kanotix.com>
-SYS=$(cut -f2 -d: /sys/devices/pci*/{,*/}*/modalias 2>&-)
+# (C) 2006 Stefan Lippers-Hollmann <s.l-h@gmx.de>
+
+SYS="$(cut -f2 -d: /sys/devices/pci*/{,*/}*/modalias 2>&-)"
 unset found found_driver
 while read id driver; do
- for sysid in $SYS; do
-  case $sysid in $id) found=$id; found_driver=$driver; break; esac
-  #if [[ $sysid == $id ]]; then found=$id; found_driver=$driver; break; fi
- done
- [ "$found" ] && break
+	for sysid in $SYS; do
+		case "$sysid" in
+			"$id")
+				found="$id"
+				found_driver="$driver"
+				break
+				;;
+		esac
+	done
+	[ "$found" ] && break
 done < <(awk -F'[\t()]' '/Server:XFree86/ && length($2)==8 {print "v0000" toupper(substr($2,0,4)) "d0000" toupper(substr($2,5,4)) "sv*sd*bc*sc*i* "$5}' /lib/discover/pci.lst)
-echo XSERVER=\"$([ -x /usr/X11R6/bin/Xorg ] && echo Xorg || echo XFree86)\"
+
+echo "XSERVER=\"Xorg\""
+
 if [ "$found" ]; then
- echo XMODULE=\"$found_driver\"
- echo XDESC=\"$(echo $(lspci -d $(echo $sysid|sed 's/sv.*//;s/v0000//;s/d0000/:/')|sed 's/.*://'))\"
+	echo "XMODULE=\"$found_driver\""
+	echo "XDESC=\"$(echo $(lspci -d $(echo $sysid|sed 's/sv.*//;s/v0000//;s/d0000/:/')|sed 's/.*://'))\""
 else
- echo XMODULE=\"vesa\"
- VGA=$(lspci|grep VGA|sed 's/.*://')
- if [ "$VGA" ]; then
-  echo XDESC=\"$(echo $VGA)\"
- else 
-  echo XDESC=\"Generic VGA card\"
- fi
-fi 
+	echo "XMODULE=\"vesa\""
+	VGA="$(lspci|grep VGA|sed 's/.*://')"
+	if [ "$VGA" ]; then
+		echo "XDESC=\"$(echo $VGA)\""
+	else 
+		echo "XDESC=\"Generic VGA card\""
+	fi
+fi
+
